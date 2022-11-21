a8559fcf84b78ad86f18b88ac947d6881f43f002
hanseongseo <seeman94@naver.com>
2022-11-21 14:32:45 +0900

    workflow main.yml 수정

---
+++ src/main/java/xcape/reservation/controller/SampleController.java
@@ -0,0 +1,14 @@
+package xcape.reservation.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/sample")
+    public String sample() {
+        return "OK";
+    }
+
+}
